855ac7654e504fa3012945b7c213d3a70be500f5
More_Vilas <viky.patil919@gmail.com>
2022-12-27 23:11:06 +0530

    Commit and push

---
+++ eclipse-workspace/Customer-Relationship-Managment-master/README.md
@@ -0,0 +1,23 @@
+# Customer-Relationship-Managment
+Customer RelationShip Management Showcases all that we need to Build a Full Stack Project Web Application.<br/><br/>
+
+
+### The Project Includes:
+    Spring MVC
+    JPA
+    JSP,CSS,JS
+    Hibernate-Orm
+    MySql,Database Scripts,Queries
+    Database - CRUD Operations
+    Aspect Oriented Programming
+    Aspect Ordering
+    @Before,@After,@AfterReturning,@Around,@AfterThrowing - Advices
+    PointCut Expressions & Combining
+    Join Points
+    
+    
+The Project Can Also Serve as a boilerplate config for Hibernate,Mysql & AOP<br/><br/>
+    
+    
+    
+    
---
+++ eclipse-workspace/Customer-Relationship-Managment-master/Web-Customer-Tracker/src/com/gk/dao/CustomerDAOImpl.java
@@ -0,0 +1,73 @@
+package com.gk.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import com.gk.entity.Customer;
+
+// Always applied to DAO implementation
+@Repository
+public class CustomerDAOImpl implements CustomerDao {
+
+	// need to inject session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	// Using Transaction so that we dont need to start and end the session
+	@Override
+	public List<Customer> getCustomers() {
+
+		// get the current Hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// create a query...sort by lastname
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
+
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+
+		// return the results
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// save/update the customer to the db
+		currentSession.saveOrUpdate(theCustomer);
+
+	}
+
+	@Override
+	public Customer getCustomers(int theId) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// get the data from the db based on the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// get the data from the db based on the primary key
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId ");
+		theQuery.setParameter("customerId", theId);
+		
+		theQuery.executeUpdate();
+	}
+
+}
